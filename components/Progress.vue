a83af40c715917da709429795d0a2fb18945bcf2
zhang13pro <lexzhang13@qq.com>
2021-12-08 17:36:57 +0800

    feat: component interactive

---
+++ components/Progress.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  count: {
+    type: Number,
+    default: 1,
+  },
+});
+const step = ref(0);
+
+function nextStep() {
+  step.value += 100 / props.count;
+}
+</script>
+
+<template>
+  <span class="uppercase">pre</span>
+  <progress @click="nextStep" :value="step" max="100"></progress>
+  <span class="uppercase">post</span>
+</template>
